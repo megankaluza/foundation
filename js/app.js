1099811a647968a21e92b0d2d92a24001b6ebf9b
Megan Kaluza and Tam Nguyen <megan@>
2016-12-01 14:59:22 -0800

    add jquery to fade in and out

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation();
+// $(document).foundation();
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".covervid-video").fadeOut(1000);
+        $("#section").fadeIn(2000);
+    });
+});
