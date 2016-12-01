055479cac06b78870290a247d6b785598612baeb
Megan Kaluza and Tam Nguyen <tam@>
2016-12-01 15:16:01 -0800

    add second page/add sass hover logo

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 $(document).ready(function(){
     $("button").click(function(){
         $(".covervid-video").fadeOut(1000);
+        $(".arrow").hide(2000);
         $("#section").fadeIn(2000);
+    });
+    $("#logo").click(function() {
+      location.href="index.html";
     });
 });
